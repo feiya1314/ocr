3c25f5c1cc5b9cac36cdab74d35671ba3da4d708
feiya1314 <yufei1313@gmail.com>
2022-04-20 22:57:26 +0800

    nginx配置和打包配置

--- ocr-service/require_by_order.sh
+++ ocr-service/require_by_order.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 for line in `cat ./requirements.txt`
-
+yum install mesa-libGL
 do
    echo print $line
    pip3 install $line
--- ocr-web/vue.config.js
+++ ocr-web/vue.config.js
@@ -16,14 +16,14 @@ module.exports = {
     configureWebpack: (process.env.NODE_ENV === 'production') ? {
         plugins: [
             new CompressionPlugin({
-                test: /\.(js|css)?$/i, // 哪些文件要压缩
+                test: /\.(js|css|svg)?$/i, // 哪些文件要压缩
                 filename: '[path].gz[query]',
                 // 压缩后的文件名
                 algorithm: 'gzip',
                 // 使用gzip压缩
                 minRatio: 1,
                 // 压缩率小于1才会压缩
-                deleteOriginalAssets: true // 删除未压缩的文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false
+                deleteOriginalAssets: false // 删除未压缩的文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false
             })
         ]
     } : {},
