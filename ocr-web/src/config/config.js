d934b2574538440947d5958b5a242807bb51ab26
feiya1314 <yufei1313@gmail.com>
2022-04-28 23:41:02 +0800

    修改url

--- ocr-web/src/config/config.js
+++ ocr-web/src/config/config.js
@@ -1,6 +1,6 @@
 // env 中的配置必须要 VUE_APP_ 开头
 
-let homeUrl = process.env.VUE_APP_OCR_BASE_URL;
+let homeUrl = process.env.VUE_APP_OCR_BASE_DOMAIN_URL;
 let ocrServiceUrl = process.env.VUE_APP_OCR_SERVICE_API;
 
 export default {
