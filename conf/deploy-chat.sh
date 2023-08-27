2bff282b7456d91c724e46729872fa356216547c
feiya1314 <yufei1313@gmail.com>
2023-08-27 21:28:34 +0800

    update deploy sh

--- conf/deploy-chat.sh
+++ conf/deploy-chat.sh
@@ -4,13 +4,25 @@
 # 将打包好的后端服务，放到/ home/ocr目录
 # 执行该脚本
 ###################################################
+ENV=$1
 deploy_file='easy-chat-service-bin.tar.gz'
 chat_dir=/home/ocr/ai-chat
 service_dir=/home/ocr/ai-chat/easy-chat-service
 service_bak_dir=/home/ocr/ai-chat/easy-chat-service-bak
-if [ ! -d "/home/ocr/ai-chat" ]; then
+
+if [ X$ENV = 'Xtest' ]; then
+  chat_dir=/home/ocr/ai-chat-test
+  service_dir=/home/ocr/ai-chat-test/easy-chat-service
+  service_bak_dir=/home/ocr/ai-chat-test/easy-chat-service-bak
+fi
+
+echo chat_dir $chat_dir
+echo service_dir $service_dir
+echo service_bak_dir $service_bak_dir
+
+if [ ! -d "$chat_dir" ]; then
   echo "web dir not exist"
-  mkdir /home/ocr/ai-chat
+  mkdir $chat_dir
 fi
 
 if [ ! -f "$deploy_file" ]; then
@@ -18,9 +30,9 @@ if [ ! -f "$deploy_file" ]; then
   exit 0
 fi
 
-if [ -d "/home/ocr/ai-chat/easy-chat-service-bak" ]; then
+if [ -d "$service_bak_dir" ]; then
   echo "web bak dir exist"
-  rm -rf /home/ocr/ai-chat/easy-chat-service-bak
+  rm -rf $service_bak_dir
 fi
 
 echo "start deploy"
